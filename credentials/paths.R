0186946d24f1047dfa5149ad09637888cbdc73e2
Topher Huebel <43896938+topherhuebel@users.noreply.github.com>
2022-08-24 12:57:10 +0100

    CH update gitignore

--- credentials/paths.R
+++ credentials/paths.R
@@ -1,5 +1,3 @@
-filepath_edgi_cleaned <- "/Users/helenadavies/King's College London/MT-TNG BioResource EDIT - ilovedata - ilovedata/data/latest_freeze/edgi/"
+filepath_edgi_cleaned <- "/Users/christopherhuebel/Library/CloudStorage/OneDrive-SharedLibraries-King'sCollegeLondon/MT-TNG BioResource EDIT - ilovedata - ilovedata/data/latest_freeze/edgi/"
 
-filepath_functions <- "/Users/helenadavies/werk/ilovedata/scripts/functions/"
-
-filepath_edgi_raw <- "/Users/helenadavies/King's College London/MT-TNG BioResource EDIT - ilovedata - ilovedata/data_raw/latest_freeze/edgi/"
+filepath_edgi_raw <- "/Users/christopherhuebel/Library/CloudStorage/OneDrive-SharedLibraries-King'sCollegeLondon/MT-TNG BioResource EDIT - ilovedata - ilovedata/data_raw/latest_freeze/edgi/"
