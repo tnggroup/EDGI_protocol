8c55ab920f3cd82d69f7e6f07402355251d6614b
Topher Huebel <43896938+topherhuebel@users.noreply.github.com>
2022-08-24 12:22:47 +0100

    CH updated credentials

--- credentials/paths.R
+++ credentials/paths.R
@@ -1,3 +1,5 @@
 filepath_edgi_cleaned <- "/Users/helenadavies/King's College London/MT-TNG BioResource EDIT - ilovedata - ilovedata/data/latest_freeze/edgi/"
+
 filepath_functions <- "/Users/helenadavies/werk/ilovedata/scripts/functions/"
+
 filepath_edgi_raw <- "/Users/helenadavies/King's College London/MT-TNG BioResource EDIT - ilovedata - ilovedata/data_raw/latest_freeze/edgi/"
